ee3f1e90dfab46b0dfeb5acf4a5fbf92aeb641df
Mirage <2159272804@qq.com>
2024-09-12 12:30:47 +0800

    Merge branch 'master' of https://gitee.com/MirageClouds/data-structure-and-algorithms

--- main.c
+++ main.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
 int main(void) {
-    printf("Hello, World!\n");
+    int sum = 0;
+    for (int i = 0; i <= 100; ++i) {
+        sum += i;
+    }
+
+    printf("sum = %d\n", sum);
+    printf("sum = %d\n", (1+100)*100/2);
     return 0;
 }
