f38211bb566ebf54511821fdffce76b6399e941b
MirageClouds <2159272804@qq.com>
2024-09-12 12:24:56 +0800

    day 1

---
+++ cmake-build-debug/CMakeFiles/edit_cache.dir/utility.sh
@@ -0,0 +1,4 @@
+set -e
+
+cd /cygdrive/c/Users/21592/CLionProjects/data-structure-and-algorithms/cmake-build-debug
+/cygdrive/c/Users/21592/AppData/Local/JetBrains/CLion2024.2/cygwin_cmake/bin/cmake.exe -E echo No\ interactive\ CMake\ dialog\ available.
---
+++ cmake-build-debug/CMakeFiles/rebuild_cache.dir/utility.sh
@@ -0,0 +1,4 @@
+set -e
+
+cd /cygdrive/c/Users/21592/CLionProjects/data-structure-and-algorithms/cmake-build-debug
+/cygdrive/c/Users/21592/AppData/Local/JetBrains/CLion2024.2/cygwin_cmake/bin/cmake.exe --regenerate-during-build -S$(CMAKE_SOURCE_DIR) -B$(CMAKE_BINARY_DIR)
